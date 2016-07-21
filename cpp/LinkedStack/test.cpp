5f55a7dc6e7f4c434152ea97ba94165d10bd870b
Abdullah Khan <khanabdullahj@gmail.com>
2016-07-21 19:45:58 -0400

    Made test simpler.

--- cpp/LinkedStack/test.cpp
+++ cpp/LinkedStack/test.cpp
@@ -18,23 +18,24 @@ int main()
 	assert( a.size() == 0 ); // If the size isn't 0, the program stops here.
 	a.push(23);         // Add 23 to a.
 	assert( a.size() == 1 );
-	cout << a.top() << endl;
+	cout << "Top: " << a.top() << endl;
 	for(size_t i = 0; i < 10; i++)
 	{
-		assert( a.size() == i + 1 );
+		assert( a.size() == i + 1);
 		a.push(i);
 	} // LinkedStack a should now be of size 11.
 
-	assert( a.size() == 11);
-
-	cout << a.pop() << endl;
-	assert( a.size() == 10);
-	int removed{a.pop()};
-	assert ( a.size () == 9);
-	cout << removed << endl;
-
 	cout << "Printing out as we pop." << endl;
-	while (!a.empty())
-		cout << a.pop() << endl;
+	while (a.size() > 0)
+	{
+		cout << "Top: " << a.top() << endl;
+		a.pop();
+	}
+
+	cout << "Creating Stack `b` of size 5 with values: 1.57, 3.14, 6.12, 6.67, 10.24 via {}-list." << endl;
+	LinkedStack<double> b{1.57, 3.14, 6.12, 6.67, 10.24};
+	assert( b.size() == 5 );
+	cout << "Size of stack b: " << b.size() << endl;
+
 	return 0;
 }
