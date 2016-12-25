515084dc689d64ba35378b16add62ef80330b228
Abdullah Khan <khanabdullahj@gmail.com>
2016-12-25 12:38:12 -0500

    No raw iterators.

--- cpp/Algorithms/selection_sort_iterators.cpp
+++ cpp/Algorithms/selection_sort_iterators.cpp
@@ -11,18 +11,18 @@ using namespace std;
 int main(int argc, char ** argv)
 {
 	vector<int> v(5); // vector of 5 elements initialized to 0.
-	helper::randomize(v.begin(), v.end());
+	helper::randomize(v);
 	cout << "Randomizing vector.\n";
 	helper::print_container(v);
-	helper::selection_sort(v.begin(), v.end());
+	algorithm::selection_sort(v);
 	cout << "Sorted vector.\n";
 	helper::print_container(v);
 	
 	array<int, 5> a = {}; // Initializes all elements to 0.
 	cout << "\nRandomizing array.\n";
-	helper::randomize(a.begin(), a.end());
+	helper::randomize(a);
 	helper::print_container(a);
-	helper::selection_sort(a.begin(), a.end());
+	algorithm::selection_sort(a);
 	cout << "Sorted array.\n";
 	helper::print_container(a);
 
