50ba22742587d99cb07d53738a189c07021918c0
Abdullah Khan <khanabdullahj@gmail.com>
2016-12-25 12:37:39 -0500

    Create new namespace for algorithms. Template containers instead of iterators.

--- cpp/Algorithms/Helper.hpp
+++ cpp/Algorithms/Helper.hpp
@@ -13,48 +13,50 @@
 #include <utility>
 #include <iostream>
 
-namespace helper {
+	namespace helper {
 
 using Generator = std::mt19937; // mersenne_twister_engine for random numbers
 using Distribution = std::uniform_int_distribution<>; // Inclusive distribution range.
 
 // Fills any given container with random integers [10,99]
 // Should only be used with containers that can hold integers.
-template<typename Iterator>
-void randomize(Iterator begin, Iterator end) {
+template<typename Container>
+void randomize(Container& c) {
 	Generator g;
 	g.seed(std::random_device()());
 	Distribution d(10,99);
-	for (auto it = begin; it != end; ++it) {
+	for (auto it = c.begin(); it != c.end(); ++it) {
 		*it = d(g);
 	}
-}
-
-// Sort basic containers of any type.
-template<typename Iterator>
-void selection_sort(Iterator begin, Iterator end) {
-	for (auto it = begin; it != end; ++it) {
-		// Using std::min_element during an interview might be cheating.
-		auto minimum_element_iterator = std::min_element(it, end);
-		std::swap(*minimum_element_iterator, *it);
-	}
-	// for (auto i = begin; i != end; ++i) {
-	// 	auto minimum_element_iterator = i;
-	// 	for (auto j = begin++; j != end; ++j) {
-	// 		if (*j < *minimum_element_iterator) {
-	// 			minimum_element_iterator = j;
-	// 		}
-	// 	}
-	// 	if (*minimum_element_iterator != *i) {
-	// 		std::swap(*minimum_element_iterator, *i);
-	// 	}
-	// }
 }
 
 // Print the contents of any single-value container.
 template<typename Container>
 void print_container(const Container& c) {
 	for (const auto i : c) { std::cout << i << ' '; }
-	std::cout << '\n';
+		std::cout << '\n';
 }
 } // namespace helper
+
+namespace algorithm {
+	// Sort basic containers of any type.
+	template<typename Container>
+	void selection_sort(Container& c) {
+		for (auto it = c.begin(); it != c.end(); ++it) {
+		// Using std::min_element during an interview might be cheating.
+			auto minimum_element_iterator = std::min_element(it, c.end());
+			std::swap(*minimum_element_iterator, *it);
+		}
+		// for (auto i = begin; i != end; ++i) {
+		// 	auto minimum_element_iterator = i;
+		// 	for (auto j = begin++; j != end; ++j) {
+		// 		if (*j < *minimum_element_iterator) {
+		// 			minimum_element_iterator = j;
+		// 		}
+		// 	}
+		// 	if (*minimum_element_iterator != *i) {
+		// 		std::swap(*minimum_element_iterator, *i);
+		// 	}
+		// }
+	}
+} // namespace algorithm
