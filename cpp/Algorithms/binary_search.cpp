7399dbe14a16bad94ef263d3882eac8cbb018d07
Abdullah Khan <khanabdullahj@gmail.com>
2017-01-17 09:33:30 -0500

    Show impl of binary search

---
+++ cpp/Algorithms/binary_search.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <random>
+#include <iostream>
+#include "Helper.hpp"
+using namespace helper;
+using namespace algorithm;
+
+int main() {
+	std::vector<int> v(3);
+	for (unsigned i = 0; i < v.size(); ++i) {
+		v[i] = i;
+	}
+	print_container(v);
+	std::cout << binary_search(v, 7) << '\n';
+	return 0;
+}
