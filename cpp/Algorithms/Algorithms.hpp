f650634658b453c4d511270988fe1cbe826acb6e
Abdullah Khan <khanabdullahj@gmail.com>
2017-03-14 11:42:10 -0400

    Add partition and quickselect

--- cpp/Algorithms/Algorithms.hpp
+++ cpp/Algorithms/Algorithms.hpp
@@ -55,4 +55,37 @@ namespace algorithm {
 
 		return -1 * (low + 1); // Key not found.
 	}
+
+	template <typename Container>
+	int partition(Container& c, int left, int right, int pivot_index)
+	{
+		auto pivot = c[pivot_index]; // Get a pivot
+		std::swap(c[pivot_index], c[right]); // Move it to the end.
+		int store_index = left;
+		for (int i = left; i < right; ++i) {
+			if (c[i] < pivot) {
+				std::swap(c[store_index], c[i]);
+				++store_index;
+			}
+		}
+		std::swap(c[right], c[store_index]); // Move pivot to it's final place.
+		return store_index;
+	}
+
+	template <typename Container>
+	// Returns the k-th smallest element.
+	auto quick_select(Container &c, int left, int right, int k) {
+		if (left == right) { // Only one element.
+			return c[left];
+		}
+		int pivot_index = ((right - left) >> 1) + left;
+		pivot_index = partition(c, left, right, pivot_index);
+		if (k == pivot_index) {
+			return c[k];
+		} else if (k < pivot_index) {
+			return quick_select(c, left, pivot_index - 1, k);
+		} else {
+			return quick_select(c, pivot_index + 1, right, k);
+		}
+	}
 } // namespace algorithm
