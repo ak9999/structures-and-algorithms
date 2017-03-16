5c8039459761fe623e623045999238a9e846dc45
Abdullah Khan <khanabdullahj@gmail.com>
2017-03-16 14:12:57 -0400

    Change: more verbose

--- cpp/src/binary_search.cpp
+++ cpp/src/binary_search.cpp
@@ -9,9 +9,15 @@ int main() {
 	std::vector<int> v(3);
 	std::iota(v.begin(), v.end(), 0);
 	print_container(v);
+	std::cout << "Searching for \'2\': ";
 	std::cout << binary_search(v, 2) << '\n';
+	std::cout << "Searching for \'5\': ";
+	std::cout << binary_search(v, 5) << '\n';
 	std::vector<std::string> s{"amaze", "armor", "butt"};
 	print_container(s);
+	std::cout << "Searching for \"amaze\": ";
 	std::cout << binary_search(s, "amaze") << '\n';
+	std::cout << "Searching for \"fight\": ";
+	std::cout << binary_search(s, "fight") << '\n';
 	return 0;
 }
