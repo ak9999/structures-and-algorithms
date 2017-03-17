fa9242f0ccd1056e9a80f34189b7ae430ed0b178
Abdullah Khan <khanabdullahj@gmail.com>
2017-03-17 19:38:11 -0400

    Completed skeleton of Vector?

--- cpp/DataStructures/Vector/Vector.hpp
+++ cpp/DataStructures/Vector/Vector.hpp
@@ -1,37 +1,69 @@
 // Abdullah Khan
 // File: Vector.hpp
 // STL-like vector.
+// Drawing ideas from what I learned in implementing Array and the repo below:
+// https://github.com/lnishan/vector
 
 #pragma once
 
 #include <memory>
+#include <utility>
+#include <iterator>
 
 template <typename T>
 class Vector {
-private:
-// TODO
 public:
-    Vector(); // Default c-tor
-    ~Vector() = default; // Destructor.
+	// Types:
+	using value_type = T;
+	using size_type = size_t;
+	using reference = T&;
+	using const_reference = const T&;
+	using pointer = T*;
+	using const_pointer = const T*;
+	using iterator = T*;
+	using const_iterator = const T*;
 
-    // Public accessors.
-    T& operator[] (size_t); // Accessor operator? I don't know what it's called.
-    T const& operator[] (size_t) const; // const version
+	// Constructors
+	Vector(); // Default c-tor
+	explicit Vector(size_type size);
+	Vector(const Vector&);					// copy-constructor
+	Vector& operator=(const Vector&);		// copy-assignment
+	Vector(Vector&&);						// move-constructor
+	Vector& operator=(Vector&&);			// move-assignment
+	~Vector() = default;					// destructor
 
-    // Public operations.
-    int size() const; // Return size.
-    T* data(); // Return underlying array.
+	// Element access
+	reference operator[] (size_type);
+	const_reference operator[] (size_type) const;
+	reference front();
+	const_reference front() const;
+	reference back();
+	const_reference back() const;
 
-    // Iterators
-    // using iterator = T*;
-    // iterator begin() { return &array_.get()[0]; }
-    // iterator end() { return &array_.get()[size_]; }
+	// Capacity.
+	bool empty();
+	size_type size() const;
 
-    // using const_iterator = const T*;
-    // const_iterator begin() const { return &array_.get()[0]; }
-    // const_iterator end() const { return &array_.get()[size_]; }
-    // const_iterator cbegin() const { return begin(); }
-    // const_iterator cend() const { return end(); }
+	// Raw data access
+	pointer data();
+	const_pointer data() const;
+
+	// Modifiers
+	void push_back(const T&);
+	void pop_back();
+	void clear();
+
+	// Iterator functions
+	iterator begin() { return &array_.get()[0]; }
+	iterator end() { return &array_.get()[size_]; }
+	const_iterator begin() const { return &array_.get()[0]; }
+	const_iterator end() const { return &array_.get()[size_]; }
+	const_iterator cbegin() const { return begin(); }
+	const_iterator cend() const { return end(); }
+private:
+	std::unique_ptr<T[]> array;
+	size_type vector_size = 0;
+	size_type reserved_size = 0;
 };
 
 #include "Vector.cpp"
